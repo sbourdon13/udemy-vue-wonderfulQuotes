38a3a98b596528e2d594c6b34b8fae4dea65d479
sbourdon13 <sbourdon13@hotmail.fr>
2019-03-21 14:58:03 +0100

    restructure project

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,9 +1,26 @@
 <template>
   <div class="mt-4">
     <h1>Quotes Added</h1>
+    <div class="progress">
+      <div
+        class="progress-bar"
+        role="progressbar"
+        :style="{ width: progressBarWidth + '%' }"
+        :aria-valuenow="progressBarWidth"
+        aria-valuemin="0"
+        aria-valuemax="100"
+      >{{ quoteCount }} / {{ maxQuotes }}</div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["quoteCount", "maxQuotes"],
+  computed: {
+    progressBarWidth() {
+      return (this.quoteCount / this.maxQuotes) * 100;
+    }
+  }
+};
 </script>
